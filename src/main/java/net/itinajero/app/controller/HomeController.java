8b8ad5e0b6b02ab289389051763a7a552cd7cb26
Alejandro <ahernandezdotnet@gmail.com>
2019-09-27 21:56:04 -0500

    Seccion 4 terminada
    * Configuracion del Proyecto
    * Creación del controlador de peliculas
    * Integracion de estilos de bootstrap

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -1,15 +1,104 @@
 package net.itinajero.app.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import net.itinajero.app.model.Pelicula;
 
 @Controller
 public class HomeController {
-
-	@RequestMapping(value="/home", method=RequestMethod.GET)
+	
+/*	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String goHome(){
 		return "home";
 	}
+*/
+	@RequestMapping(value="/")
+	public String mostrarPrincipal(Model model) {
+		model.addAttribute("peliculas", getLista());
+		
+		return "home";
+		
+	}
+
+	@RequestMapping(value="/detail")
+	public String mostrarDetalle(Model model) {				
+		String titulo = "Rapido y Furioso";
+		int duracion = 123;
+		double precio = 50;
+		
+		model.addAttribute("titulo", titulo);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precio", precio);
+				
+		
+		return "detalle";
+		
+	}
+	
+	private List<Pelicula> getLista(){
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		
+		List<Pelicula> peliculas = new LinkedList<Pelicula>();
+		try {
+		
+			Pelicula pelicula1 = new Pelicula();
+			pelicula1.setId(1);
+			pelicula1.setTitulo("Power Rangers");
+			pelicula1.setDuracion(120);
+			pelicula1.setClasificacion("B");
+			pelicula1.setGenero("Aventura");
+			pelicula1.setFechaEstreno(formatter.parse("02-05-2017"));
+			// imagen="cinema.png"
+			// estatus="Activa"
+
+			Pelicula pelicula2 = new Pelicula();
+			pelicula2.setId(2);
+			pelicula2.setTitulo("La bella y la bestia");
+			pelicula2.setDuracion(132);
+			pelicula2.setClasificacion("A");
+			pelicula2.setGenero("Infantil");
+			pelicula2.setFechaEstreno(formatter.parse("20-05-2017"));
+			pelicula2.setImagen("bella.png"); // Nombre del archivo de imagen
+
+			Pelicula pelicula3 = new Pelicula();
+			pelicula3.setId(3);
+			pelicula3.setTitulo("Contratiempo");
+			pelicula3.setDuracion(106);
+			pelicula3.setClasificacion("B");
+			pelicula3.setGenero("Thriller");
+			pelicula3.setFechaEstreno(formatter.parse("28-05-2017"));
+			pelicula3.setImagen("contratiempo.png"); // Nombre del archivo de imagen
+
+			Pelicula pelicula4 = new Pelicula();
+			pelicula4.setId(4);
+			pelicula4.setTitulo("Kong La Isla Calavera");
+			pelicula4.setDuracion(118);
+			pelicula4.setClasificacion("B");
+			pelicula4.setGenero("Accion y Aventura");
+			pelicula4.setFechaEstreno(formatter.parse("06-06-2017"));
+			pelicula4.setImagen("kong.png"); // Nombre del archivo de imagen
+			pelicula4.setEstatus("Inactiva"); // Esta pelicula estara inactiva
+			
+			// Agregamos los objetos Pelicula a la lista
+				
+			peliculas.add(pelicula1);
+			peliculas.add(pelicula2);
+			peliculas.add(pelicula3);
+			peliculas.add(pelicula4);
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return peliculas;
+	}
+
 	
 }
--- webapp/WEB-INF/views/home.jsp
+++ webapp/WEB-INF/views/home.jsp
@@ -1,12 +1,73 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%> 
+<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Pagina Principal</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Bienvenido a Cineapp</title>
 </head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+<spring:url value="/resources" var="urlPublic" />
 <body>
-	<h1>Bienvenido a la pagina Principal</h1>
-	Este es el home.jsp del controlador Home
+
+${urlPublic}
+<div class="panel panel-default">
+  <div class="panel-heading">Panel heading without title</div>
+  <div class="panel-body">
+    Panel content
+  </div>
+</div>
+
+	<div class="panel panel-default">
+	  <div class="panel-heading">Tabla de Peliculas</div>
+	  <div class="panel-body">
+		<table class="table table-striped table-hover table-bordered"> 
+			<thead>
+				<tr>
+					<th>Id</th>
+					<th>Titulo</th>
+					<th>Duracion</th>
+					<th>Clasificacion</th>
+					<th>Genero</th>
+					<th>Imagen</th>
+					<th>Fecha Estreno</th>
+					<th>Estatus</th>
+				</tr>
+			</thead>
+			
+			<tbody>
+				<c:forEach items="${peliculas}" var="pelicula">
+					<tr>
+						<td>${pelicula.id}</td>
+						<td>${pelicula.titulo}</td>
+						<td>${pelicula.duracion}</td>
+						<td>${pelicula.clasificacion}</td>
+						<td>${pelicula.genero}</td>
+						<td> <img src="${urlPublic}/images/${pelicula.imagen}" alt="${pelicula.titulo}" width="80px" height="100px" > </td>
+						<td>
+							<fmt:formatDate value="${pelicula.fechaEstreno}" pattern="dd-MM-yyyy"/>	
+						</td>
+						<td>
+						<c:choose>
+							<c:when test="${pelicula.estatus=='Activa'}">
+							<span class="label label-success"> ${pelicula.estatus} </span>
+							</c:when>
+							<c:otherwise>
+							<span class="label label-danger"> ${pelicula.estatus} </span>
+							</c:otherwise>
+						</c:choose>
+						</td>
+					</tr>
+				</c:forEach>	
+			
+			</tbody>
+		
+		</table>
+	  </div>
+	</div>
+	
 </body>
 </html>
